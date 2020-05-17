ba43c271f9480bb99a2065d0de0f67386830ee8b
pgalic96 <p.galic96@gmail.com>
2020-05-17 23:22:45 +0200

    fixup! console,oauth: Add error reporting to redux store

--- pkg/webui/console/store/index.js
+++ pkg/webui/console/store/index.js
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import { mapValues } from 'lodash'
 import * as Sentry from '@sentry/browser'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { createLogicMiddleware } from 'redux-logic'
@@ -25,17 +26,74 @@ import createRootReducer from './reducers'
 import requestPromiseMiddleware from './middleware/request-promise-middleware'
 import logics from './middleware/logics'
 
-Sentry.init({
-  dsn: env.sentryDsn,
-  release: process.env.VERSION,
-  normalizeDepth: 10,
-})
+if (env.sentryDsn)
+  Sentry.init({
+    dsn: env.sentryDsn,
+    release: process.env.VERSION,
+    normalizeDepth: 10,
+  })
 
 const composeEnhancers = (dev && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 
 export default function(history) {
   const middleware = applyMiddleware(
-    createSentryMiddleware(Sentry, {}),
+    createSentryMiddleware(Sentry, {
+      actionTransformer: action => {
+        switch (action.type) {
+          case 'GET_API_KEY_LIST_SUCCESS':
+          case 'GET_API_KEY_SUCCESS':
+            return {
+              ...action,
+              payload: {
+                ...action.payload,
+                entities: undefined,
+              },
+            }
+          default:
+            return action
+        }
+      },
+      stateTransformer: state => {
+        return {
+          ...state,
+          apiKeys: undefined,
+          collaborators: {
+            entities: {
+              ...state.collaborators.entities,
+              admin: {
+                ...state.collaborators.entities.admin,
+                ids: undefined,
+              },
+            },
+          },
+          devices: {
+            ...state.devices,
+            entities: mapValues(state.devices.entities, value => {
+              if (Boolean(value.ids)) {
+                return { ...value, ids: undefined }
+              }
+            }),
+          },
+          gateways: {
+            ...state.gateways,
+            entities: mapValues(state.gateways.entities, value => {
+              if (Boolean(value.ids)) {
+                return { ...value, ids: undefined }
+              }
+            }),
+          },
+          pagination: undefined,
+          user: {
+            ...state.user,
+            user: {
+              ...state.user.user,
+              ids: undefined,
+            },
+          },
+          users: undefined,
+        }
+      },
+    }),
     requestPromiseMiddleware,
     routerMiddleware(history),
     createLogicMiddleware(logics),
